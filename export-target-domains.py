6701fc549b28634cec820d3bfa1445c991e75102
Rahul B <rahulbot@gmail.com>
2021-11-01 16:17:48 -0400

    first pass at new export script for target domains

--- export-target-domains.py
+++ export-target-domains.py
@@ -8,14 +8,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(
 logger = logging.getLogger(__name__)
 
 INPUT_DIR = "input"
-OUTPUT_DIR = os.path.join("export", "by_media")
+OUTPUT_DIR = os.path.join("export", "tmp")
 RUN_PARALLEL = True
 
 
 def media_dir_export_worker(dir: str) -> None:
     logging.info("Working on {}".format(dir))
-    export_file_name = dir.split("/")[-1] + ".ndjson"
-    logging.info("  Export to {}".format(export_file_name))
     # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
     files = [os.path.join(dir, f) for f in os.listdir(dir)
              if os.path.isfile(os.path.join(dir, f)) and f.endswith('.json')]
@@ -23,14 +21,20 @@ def media_dir_export_worker(dir: str) -> None:
         logging.info("  Skipping - no files")
         return
     logging.info("  Found {} story files".format(len(files)))
+    # find data
+    all_target_domains = {}
+    for story_file in files:
+        with open(story_file) as f2:
+            story = json.load(f2)
+        story_target_domains = set([link['target_domain'] for link in story['links']])
+        all_target_domains = set.union(all_target_domains, story_target_domains)
+    # write output
+    export_file_name = dir.split("/")[-1] + "-targets.txt"
+    logging.info("  Export {} target domains to {}".format(len(all_target_domains), export_file_name))
     with io.open(os.path.join(OUTPUT_DIR, export_file_name), 'w', encoding='utf8') as output_file:
-        for story_file in files:
-            with open(story_file) as f2:
-                story = json.load(f2)
-            for link in story['links']:
-                link['publication_date'] = link['publication_date'][0:19]  # strip off milliseconds
-                output_file.write(json.dumps(link, ensure_ascii=False)+"\n")
-    logging.info("  Finished {} stories from {}".format(len(files), dir))
+        for item in all_target_domains:
+            output_file.write(item + "\n")
+    logging.info("  Finished {} stories from {} - {} target domains".format(len(files), dir, len(all_target_domains)))
 
 
 if __name__ == '__main__':
